1dc17b3d845392ddcf6650dd3271e8d66cc89e7b
NSSMac2009EC <nssmac2009ec@NSSMac2009ECs-MacBook-Pro.local>
2018-07-17 13:21:29 -0500

    revised js to use document fragment

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Practice creating elements featuring Tommy Wiseau
 
 ### Put an article DOM element in your index.html with the id attribute value of messages.
+
 ### In your JavaScript, use querySelector() to obtain a reference to that article DOM element.
+
 ### Create five (5) section elements, each with a class of message, and with textContent of your choosing.
+
 ### Using appendChild(), attach each message as a child to the messages element.
+
+# Part 2 
+
+###Update your code from the previous exercise to add the chat messages to the messages elements via a document fragment.
--- main.js
+++ main.js
@@ -1,32 +1,41 @@
-const messages = document.querySelector('article');
+// const messages = document.querySelector('article');
+
+const fragment = document.createDocumentFragment();
 
 const notTrue = document.createElement('section');
-notTrue.className = "message"
-notTrue.textContent = "I did not hit her, it's not true!"
+// notTrue.className = "message"
+notTrue.textContent = "I did not hit her, it's not true!";
+fragment.appendChild(notTrue);
 
 const bullshit = document.createElement('section');
-bullshit.className = "message"
-bullshit.textContent = "It's bullshit!"
+// bullshit.className = "message"
+bullshit.textContent = "It's bullshit!";
+fragment.appendChild(bullshit);
 
 const hit = document.createElement('section');
-hit.className = "message"
-hit.textContent = "I did not hit her!"
+// hit.className = "message"
+hit.textContent = "I did not hit her!";
+fragment.appendChild(hit);
 
 const naht = document.createElement('section');
-naht.className = "message"
-naht.textContent = "I did NAAAHHHT!"
+// naht.className = "message"
+naht.textContent = "I did NAAAHHHT!";
+fragment.appendChild(naht);
 
 const Mark = document.createElement('section');
-Mark.className = "message"
-Mark.textContent = "Oh hi mark."
+// Mark.className = "message"
+Mark.textContent = "Oh hi mark.";
+fragment.appendChild(Mark);
 
-messages.appendChild(notTrue)
-messages.appendChild(bullshit)
-messages.appendChild(hit)
-messages.appendChild(naht)
-messages.appendChild(Mark)
+// messages.appendChild(notTrue)
+// messages.appendChild(bullshit)
+// messages.appendChild(hit)
+// messages.appendChild(naht)
+// messages.appendChild(Mark)
 
-console.log(messages)
+document.querySelector("article").appendChild(fragment);
+
+console.log(messages);
 
 
 
